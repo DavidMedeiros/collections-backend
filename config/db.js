1eb50f86895eab23739a37b621613b8f7ea31d09
David Souza <david.souza@ccc.ufcg.edu.br>
2018-12-12 22:07:57 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds143683.mlab.com:43683/fanfic-p3`, 
+    url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds123434.mlab.com:23434/discollection`, 
     local_url: "mongodb://localhost/collections-dev"
 }
