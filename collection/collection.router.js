c23a887820244ba0b47fad984111035f369166a5
David <david.souza@ccc.ufcg.edu.br>
2018-12-12 23:12:57 -0300

    :cry: adding protection to collection route

--- collection/collection.router.js
+++ collection/collection.router.js
@@ -3,20 +3,27 @@ var router               = express.Router();
 var RequestStatus        = require('../constants/requestStatus');
 var collectionController = require('./collection.controller');
 
- 
+function checkAuthentication(req,res,next){
+  console.log(req.session);
+  if(req.isAuthenticated()){
+    next();
+  } else{
+    res.status(RequestStatus.UNAUTHORIZED).send('User not logged.');
+  }
+}
 
-router.get('/',  collectionController.index);
+router.get('/', checkAuthentication,  collectionController.index);
 
-router.get('/:collection_id', collectionController.show);
+router.get('/:collection_id', checkAuthentication, collectionController.show);
 
-router.post('/', /*checkAuthentication,*/ collectionController.create);
+router.post('/', checkAuthentication, collectionController.create);
 
-router.put('/:collection_id', collectionController.update);
+router.put('/:collection_id', checkAuthentication, collectionController.update);
 
-router.delete('/:collection_id', collectionController.delete);
+router.delete('/:collection_id', checkAuthentication, collectionController.delete);
 
-router.put('/:collection_id/album', collectionController.addAlbum);
+router.put('/:collection_id/album', checkAuthentication, collectionController.addAlbum);
 
-router.delete('/:collection_id/album/:album_id', collectionController.removeAlbum);
+router.delete('/:collection_id/album/:album_id', checkAuthentication, collectionController.removeAlbum);
 
 module.exports = router;
