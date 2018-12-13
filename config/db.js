ac20e133df34f73eef4d1cd5d022406f738455b8
David Souza <david.souza@ccc.ufcg.edu.br>
2018-12-12 22:05:50 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
+    url: mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds123434.mlab.com:23434/discollection
     local_url: "mongodb://localhost/collections-dev"
 }
